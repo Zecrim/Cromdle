2be9adc80df4ffeab0b11522746eccef43cd7318
Zecrim <samuel.thomas424@gmail.com>
2024-04-25 22:23:21 -0500

    added starting and ending modals

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ function $(cssSelector) {
 function $$(cssSelector) {
   return document.querySelectorAll(cssSelector)
 }
+const modal = $('.modal-container')
+const winModalEl = $('#win')
 const guesses = $('.guesses')
+const startBtn = $('#start')
+const againBtn = $('#again')
 const guessBtn = $('#submit')
 const letters1 = $$('.letter1')
 const letters2 = $$('.letter2')
@@ -16,7 +20,8 @@ const letters4 = $$('.letter4')
 const letters5 = $$('.letter5')
 const letters6 = $$('.letter6')
 
-const wins = 0
+let wins = 0
+let turn = 0
 let letters = ''
 let letter1 = ''
 let letter2 = ''
@@ -25,28 +30,40 @@ let letter4 = ''
 let letter5 = ''
 const yellow = 'rgb(181, 159, 59)'
 const green = 'rgb(83, 141, 78)'
+const wallColor = 'rgb(38, 42, 48)'
 let currentGuess = ['', '', '', '', '']
-let currentAnswer = answerList[Math.floor(Math.random() * answerList.length)].split('')
-console.log(currentAnswer)
-let turn = 1
+let currentAnswer = ''
 
+// Lets you type smoothly by putting focus on the next or previous element based on what you press
 guesses.addEventListener('input', function(e){
     if ( e.target.value !== "" && e.target.nextElementSibling && e.target.nextElementSibling.nodeName === "INPUT" ){
 
         e.target.nextElementSibling.focus();
-    
-      } else if (e.target.value === "" && e.target.previousElementSibling && e.target.previousElementSibling.nodeName === "INPUT") {
-        
-        e.target.previousElementSibling.focus();
-
       }
 }) 
+guesses.addEventListener('keydown', function(e){
+  if (e.key == "Backspace" && e.target.value === "" && e.target.previousElementSibling && e.target.previousElementSibling.nodeName === "INPUT") {
+      
+      e.target.previousElementSibling.focus();
+    }
+}) 
 
-    guessBtn.addEventListener('click', submitGuess)
+function closeModal() {
+  if (turn == 0) {turn++}
+  console.log(turn)
+  modal.classList.remove('show')
+}
 
+function winModal() {
+  if (letter1 === currentAnswer[0] && letter2 === currentAnswer[1] && letter3 === currentAnswer[2] && letter4 === currentAnswer[3] && letter5 === currentAnswer[4]) {
+    console.log('you win!')
+    wins++
+    winModalEl.classList.add('show')
+}
+}
 
-
-function submitGuess () {
+//the function that gets played when you make a guess
+function submitGuess() {
   assignLetters()
   wordList.forEach((word) => {
     if (letter1 + letter2 + letter3 + letter4 + letter5 === word) {
@@ -111,83 +128,103 @@ function assignLetters() {
 
 function disableBoxes() {
   if(turn === 1) {
-    letters1[0].disabled = true
-    letters1[1].disabled = true
-    letters1[2].disabled = true
-    letters1[3].disabled = true
-    letters1[4].disabled = true
-  } else if (turn === 2) {
-    letters2[0].disabled = true
-    letters2[1].disabled = true
-    letters2[2].disabled = true
-    letters2[3].disabled = true
-    letters2[4].disabled = true
-  } else if (turn === 3) {
-    letters3[0].disabled = true
-    letters3[1].disabled = true
-    letters3[2].disabled = true
-    letters3[3].disabled = true
-    letters3[4].disabled = true
-  } else if (turn === 4) {
-    letters4[0].disabled = true
-    letters4[1].disabled = true
-    letters4[2].disabled = true
-    letters4[3].disabled = true
-    letters4[4].disabled = true
-  } else if (turn === 5) {
-    letters5[0].disabled = true
-    letters5[1].disabled = true
-    letters5[2].disabled = true
-    letters5[3].disabled = true
-    letters5[4].disabled = true
-  } else if (turn === 6) {
-    letters6[0].disabled = true
-    letters6[1].disabled = true
-    letters6[2].disabled = true
-    letters6[3].disabled = true
-    letters6[4].disabled = true
+    letters1.forEach((letter) => {letter.disabled = true})
+    letters2.forEach((letter) => {letter.disabled = false})
+  } if (turn === 2 || turn === 0) {
+    letters2.forEach((letter) => {letter.disabled = true})
+    letters3.forEach((letter) => {letter.disabled = false})
+  } if (turn === 3 || turn === 0) {
+    letters3.forEach((letter) => {letter.disabled = true})
+    letters4.forEach((letter) => {letter.disabled = false})
+  } if (turn === 4 || turn === 0) {
+    letters4.forEach((letter) => {letter.disabled = true})
+    letters5.forEach((letter) => {letter.disabled = false})
+  } if (turn === 5 || turn === 0) {
+    letters5.forEach((letter) => {letter.disabled = true})
+    letters6.forEach((letter) => {letter.disabled = false})
+  } if (turn === 6 || turn === 0) {
+    letters6.forEach((letter) => {letter.disabled = true})
   }
 }
 
 function checkAnswer() {
   // if (!letter1.disabled && !letter2.disabled && !letter3.disabled && !letter4.disabled && !letter5.disabled) {
     //chekcs to see if any yellow letters
-    currentAnswer.forEach((answerLetter) => {
-      if (letter1 === answerLetter ) {
-        letters[0].style.backgroundColor = yellow
-      }
-      if (letter2 === answerLetter ) {
-        letters[1].style.backgroundColor = yellow
-      }
-      if (letter3 === answerLetter ) {
-        letters[2].style.backgroundColor = yellow
-      }
-      if (letter4 === answerLetter ) {
-        letters[3].style.backgroundColor = yellow
-      }
-      if (letter5 === answerLetter ) {
-        letters[4].style.backgroundColor = yellow
-      }  
-  })
-  //checks to see if any green letters
     if (letter1 === currentAnswer[0] ) {
       letters[0].style.backgroundColor = green
+    } else if (letter1 === currentAnswer[0] || letter1 === currentAnswer[1] || letter1 === currentAnswer[2] || letter1 === currentAnswer[3] || letter1 === currentAnswer[4]) {
+      letters[0].style.backgroundColor = yellow
     }
     if (letter2 === currentAnswer[1] ) {
       letters[1].style.backgroundColor = green
+    } else if (letter2 === currentAnswer[0] || letter2 === currentAnswer[1] || letter2 === currentAnswer[2] || letter2 === currentAnswer[3] || letter2 === currentAnswer[4]) {
+      letters[1].style.backgroundColor = yellow
     }
     if (letter3 === currentAnswer[2] ) {
       letters[2].style.backgroundColor = green
+    } else if (letter3 === currentAnswer[0] || letter3 === currentAnswer[1] || letter3 === currentAnswer[2] || letter3 === currentAnswer[3] || letter3 === currentAnswer[4]) {
+      letters[2].style.backgroundColor = yellow
     }
     if (letter4 === currentAnswer[3] ) {
       letters[3].style.backgroundColor = green
+    } else if (letter4 === currentAnswer[0] || letter4 === currentAnswer[1] || letter4 === currentAnswer[2] || letter4 === currentAnswer[3] || letter4 === currentAnswer[4]) {
+      letters[3].style.backgroundColor = yellow
     }
     if (letter5 === currentAnswer[4] ) {
       letters[4].style.backgroundColor = green
-      
-    if (letter1 === currentAnswer[0] && letter2 === currentAnswer[1] && letter3 === currentAnswer[2] && letter4 === currentAnswer[3] && letter5 === currentAnswer[4]) {
-      console.log('you win!')
-      wins++
+    } else if (letter5 === currentAnswer[0] || letter5 === currentAnswer[1] || letter5 === currentAnswer[2] || letter5 === currentAnswer[3] || letter5 === currentAnswer[4]) {
+      letters[4].style.backgroundColor = yellow
+    }  
+    winModal()
     }
+
+  function init() {
+    reset()
+    turn = 0
+    letters = ''
+    letter1 = ''
+    letter2 = ''
+    letter3 = ''
+    letter4 = ''
+    letter5 = ''
+    disableBoxes()
+    currentAnswer = answerList[Math.floor(Math.random() * answerList.length)].split('') // decides the current answer randomly
+    console.log(currentAnswer)
+    winModalEl.classList.remove('show')
+    modal.classList.add('show')
   }
-  }
+
+  function reset() {
+    //re-enabling all the squares
+    letters1.forEach((letter) => {letter.disabled = false})
+    letters2.forEach((letter) => {letter.disabled = false})
+    letters3.forEach((letter) => {letter.disabled = false})
+    letters4.forEach((letter) => {letter.disabled = false})
+    letters5.forEach((letter) => {letter.disabled = false})
+    letters6.forEach((letter) => {letter.disabled = false})
+
+    //clearing the letters
+    letters1.forEach((letter) => {letter.value = ''})
+    letters2.forEach((letter) => {letter.value = ''})
+    letters3.forEach((letter) => {letter.value = ''})
+    letters4.forEach((letter) => {letter.value = ''})
+    letters5.forEach((letter) => {letter.value = ''})
+    letters6.forEach((letter) => {letter.value = ''})
+
+    //clearing the colors
+    letters1.forEach((letter) => {letter.style.backgroundColor = wallColor})
+    letters2.forEach((letter) => {letter.style.backgroundColor = wallColor})
+    letters3.forEach((letter) => {letter.style.backgroundColor = wallColor})
+    letters4.forEach((letter) => {letter.style.backgroundColor = wallColor})
+    letters5.forEach((letter) => {letter.style.backgroundColor = wallColor})
+
+
+    }
+
+  console.log(turn)
+
+
+  init()
+  startBtn.addEventListener('click', closeModal)
+  guessBtn.addEventListener('click', submitGuess)
+  againBtn.addEventListener('click', init)
